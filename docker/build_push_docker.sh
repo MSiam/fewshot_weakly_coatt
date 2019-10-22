ad0171fbaa1de32239735accbe833e1626e3f1b8
Boris Oreshkin <boris@elementai.com>
2019-10-22 18:45:54 +0000

    Confirmed that the docker can be built and pushed at EAI. Updated the repository location, the old one doesnt work anymore

--- docker/build_push_docker.sh
+++ docker/build_push_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker build -f Dockerfile_cluster -t fewshot_webly .
-docker tag fewshot_webly images.borgy.elementai.lan/tensorflow/fewshot_webly
-docker push images.borgy.elementai.lan/tensorflow/fewshot_webly
+docker tag fewshot_webly images.borgy.elementai.net/fewshot_webly:$USER
+docker push images.borgy.elementai.net/fewshot_webly:$USER
 chmod +x ../main.py
