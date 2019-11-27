aafdaa8448ad7b0985bf2d20ecf5dd3fa56bc1ac
Mennatullah Siam <menna@cedar5.cedar.computecanada.ca>
2019-11-27 08:39:24 -0800

    removed validation and saving last ckpt

--- train.py
+++ train.py
@@ -102,12 +102,8 @@ def meta_train(options):
 
     loss_list = [] # track training loss
     iou_list = [] # track validaiton iou
-    highest_iou = 0
     if len(snapshot_manager.losses['training']) > 0:
         loss_list = list(snapshot_manager.losses['training'].values())
-    if len(snapshot_manager.losses['validation']) > 0:
-        iou_list = list(snapshot_manager.losses['validation'].values())
-        highest_iou = np.max(iou_list)
 
     model.cuda()
     model = model.train()
@@ -115,7 +111,6 @@ def meta_train(options):
     # initialize vars for summaries and logging
     tensorboard = SummaryWriter(log_dir = os.path.join(checkpoint_dir, 'tensorboard'))
     tempory_loss = 0  # accumulated loss
-    best_epoch=0
     snapshot_manager.enable_time_tracking()
 
     for epoch in range(last_epoch+1, num_epoch):
@@ -161,91 +156,8 @@ def meta_train(options):
                 plot_loss(checkpoint_dir, loss_list, save_pred_every)
                 np.savetxt(os.path.join(checkpoint_dir, 'loss_history.txt'), np.array(loss_list))
                 tempory_loss = 0
-        # ======================evaluate now==================
-        with torch.no_grad():
-            print ('----Evaluation----')
-            model = model.eval()
-
-            best_iou = 0
-            initial_seed = options.seed #+ epoch
-            for eva_iter in range(options.iter_time):
-                if options.dataset_name == 'pascal':
-                    valset = Dataset_val(data_dir=data_dir, fold=options.fold, input_size=input_size,
-                                         normalize_mean=IMG_MEAN, normalize_std=IMG_STD,
-                                         split=options.split, seed=initial_seed+eva_iter, n_shots=options.n_shots)
-                else:
-                    valset, _ = create_coco_fewshot(data_dir, 'trainval', input_size=input_size,
-                                                 n_ways=1, n_shots=1, max_iters=1000, fold=options.fold,
-                                                 prob=options.prob, seed=initial_seed+eva_iter)
-
-                valset.history_mask_list=[None] * 1000
-                valloader = data.DataLoader(valset, batch_size=options.bs_val, shuffle=False,
-                                            num_workers=options.num_workers,
-                                            drop_last=False)
-
-                all_inter, all_union, all_predict = [0] * nfold_out_classes, [0] * nfold_out_classes, [0] * nfold_out_classes
-                for i_iter, batch in enumerate(valloader):
-                    print('Eva Iter ', i_iter)
-                    query_rgb, query_mask, support_rgb, support_mask, history_mask, _, _, sample_class, index = batch
-                    query_rgb = (query_rgb).cuda(0)
-                    support_rgb = (support_rgb).cuda(0)
-                    support_mask = (support_mask).cuda(0)
-                    query_mask = (query_mask).cuda(0).long()  # change formation for crossentropy use
-
-                    query_mask = query_mask[:, 0, :, :]  # remove the second dim,change formation for crossentropy use
-                    history_mask = (history_mask).cuda(0)
-
-                    if options.model_type == 'vanilla':
-                        pred = model(query_rgb, support_rgb, support_mask,history_mask)
-                    else:
-                        pred = model(query_rgb, support_rgb, sample_class,history_mask)
-                    pred_softmax = F.softmax(pred, dim=1).data.cpu()
-
-                    # update history mask
-                    for j in range(support_mask.shape[0]):
-                        sub_index = index[j]
-                        valset.history_mask_list[sub_index] = pred_softmax[j]
-
-                    pred = nn.functional.interpolate(pred, size=input_size, mode='bilinear',
-                                                         align_corners=True)  #upsample  # upsample
-
-                    _, pred_label = torch.max(pred, 1)
-                    inter_list, union_list, _, num_predict_list = get_iou_v1(query_mask, pred_label)
-                    for j in range(query_mask.shape[0]):#batch size
-                        all_inter[mapping[int(sample_class[j])]] += inter_list[j]
-                        all_union[mapping[int(sample_class[j])]] += union_list[j]
-
-                IOU = []
-                for j in range(nfold_out_classes):
-                    if all_union[j] != 0:
-                        IOU.append(all_inter[j] / all_union[j])
-
-                mean_iou = np.mean(IOU)
-                print('IOU:%.4f' % (mean_iou))
-                if mean_iou > best_iou:
-                    best_iou = mean_iou
-                else:
-                    break
-
-            iou_list.append(best_iou)
-            plot_iou(checkpoint_dir, iou_list)
-            np.savetxt(os.path.join(checkpoint_dir, 'iou_history.txt'), np.array(iou_list))
-
-            if best_iou>highest_iou:
-                highest_iou = best_iou
-                model = model.eval()
-                torch.save(model.cpu().state_dict(), osp.join(checkpoint_dir, 'model', 'best.pth'))
-                model = model.train()
-                best_epoch = epoch
-                print('A better model is saved')
-
-            print('IOU for this epoch: %.4f' % (best_iou))
-
-            model = model.train()
-            model.cuda()
 
         epoch_time = time.time() - begin_time
-        print('best epoch:%d ,iout:%.4f' % (best_epoch, highest_iou))
         print('This epoch takes:', epoch_time, 'second')
         print('still need hour:%.4f' % ((num_epoch - epoch) * epoch_time / 3600))
 
@@ -264,5 +176,9 @@ def meta_train(options):
         tensorboard.add_scalar('test/mean_iou', test_miou, epoch)
 
         scheduler.step()
-
+    
+    model = model.eval()
+    torch.save(model.cpu().state_dict(), osp.join(checkpoint_dir, 'model', 'best.pth'))
+    model = model.train()
+    print('A model is saved')
     tensorboard.close()
