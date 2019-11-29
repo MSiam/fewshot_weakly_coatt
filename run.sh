7ea05a4f7e4859746a93b2889a34ee7ecfd79892
Mennatullah Siam <mennatul@ualberta.ca>
2019-11-28 19:32:23 -0500

    fix bug in not saving skpt after training with 1 epoch

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 #SBATCH --job-name=fewshot_webly
 #SBATCH --gres=gpu:2
 #SBATCH --mem=5000M
-#SBATCH --time=1-14:00 #d-hh:mm
+#SBATCH --time=0-20:00 #d-hh:mm
 #SBATCH --output=outputs/webly%A%a.out
 #SBATCH --error=errors/webly%A%a.err
 
@@ -25,4 +25,4 @@ experiments=( 0 'iter_nwe_coatt' 1 'iter_nwe_coatt' 2 'iter_nwe_coatt' 3 'iter_n
 #echo Hello running $SLURM_ARRAY_TASK_ID fold ${experiments[$((($SLURM_ARRAY_TASK_ID-1)*2))]} model type ${experiments[$((($SLURM_ARRAY_TASK_ID-1)*2+1))]}
 
 source /home/menna/projects/def-jag/menna/.weblyvenv/bin/activate
-python main.py -fold ${experiments[$((($SLURM_ARRAY_TASK_ID-1)*2))]} -train 1 -test_multi_run 1 -model_type ${experiments[$((($SLURM_ARRAY_TASK_ID-1)*2+1))]} -embed_type word2vec -data_dir /home/menna/projects/def-jag/menna/VOCdevkit/VOC2012/ -dataset_name pascal -n_shots $n_shots -bs 4 -ckpt ${experiments[$((($SLURM_ARRAY_TASK_ID-1)*2+1))]} -niterations 30000
+python main.py -fold ${experiments[$((($SLURM_ARRAY_TASK_ID-1)*2))]} -train 1 -test_multi_run 1 -model_type ${experiments[$((($SLURM_ARRAY_TASK_ID-1)*2+1))]} -embed_type word2vec -data_dir /home/menna/projects/def-jag/menna/VOCdevkit/VOC2012/ -dataset_name pascal -n_shots $n_shots -bs 4 -ckpt ${experiments[$((($SLURM_ARRAY_TASK_ID-1)*2+1))]} -niterations 100000
--- run_exps.sh
+++ run_exps.sh
@@ -16,13 +16,12 @@
 ##n_shots=(1, 5)
 #for i in {0..1}
 #do
-#    for fold in {0..3}
-#    do
-#        echo 'N_SHOTS =' ${n_shots[i]} ' FOLD = ' $fold ' BS = ' ${batch_sizes[i]}
-#        python main.py -train 0 -test_multi_run 1 -model_type iter_nwe_coatt -fold $fold -embed_type word2vec -data_dir ~/Dataset/VOCdevkit/VOC2012/ -dataset_name pascal -n_shots ${n_shots[i]} -bs ${batch_sizes[i]} -ckpt checkpoints_trainval/model_type\=iter_nwe_coatt\,fold\=$fold\,seed\=1337\,lr\=0.001\,split\=trainval\,embed_type\=word2vec\,dataset_name\=pascal/testing/ -use_web 1
-#    done
+for fold in {0..3}
+do
+	python main.py -train 0 -test_multi_run 1 -model_type iter_nwe_coatt -fold $fold -embed_type word2vec -data_dir /home/menna/projects/def-jag/menna/VOCdevkit/VOC2012/ -dataset_name pascal -n_shots 1 -bs 4 -ckpt iter_nwe_coatt
+done
 #done
 
-python main.py -train 1 -test_multi_run 1 -model_type iter_nwe_coatt -fold 0 -embed_type word2vec -data_dir ~/Dataset/VOCdevkit/VOC2012/ -dataset_name pascal -n_shots 1 -bs 4 -ckpt stability_exps/test1/ -reproducability 1 -num_epoch 20
-python main.py -train 1 -test_multi_run 1 -model_type iter_nwe_coatt -fold 0 -embed_type word2vec -data_dir ~/Dataset/VOCdevkit/VOC2012/ -dataset_name pascal -n_shots 1 -bs 4 -ckpt stability_exps/test2/ -reproducability 1 -num_epoch 20
-python main.py -train 1 -test_multi_run 1 -model_type iter_nwe_coatt -fold 0 -embed_type word2vec -data_dir ~/Dataset/VOCdevkit/VOC2012/ -dataset_name pascal -n_shots 1 -bs 4 -ckpt stability_exps/test3/ -reproducability 1 -num_epoch 20
+#python main.py -train 1 -test_multi_run 1 -model_type iter_nwe_coatt -fold 0 -embed_type word2vec -data_dir ~/Dataset/VOCdevkit/VOC2012/ -dataset_name pascal -n_shots 1 -bs 4 -ckpt stability_exps/test1/ -reproducability 1 -num_epoch 20
+#python main.py -train 1 -test_multi_run 1 -model_type iter_nwe_coatt -fold 0 -embed_type word2vec -data_dir ~/Dataset/VOCdevkit/VOC2012/ -dataset_name pascal -n_shots 1 -bs 4 -ckpt stability_exps/test2/ -reproducability 1 -num_epoch 20
+#python main.py -train 1 -test_multi_run 1 -model_type iter_nwe_coatt -fold 0 -embed_type word2vec -data_dir ~/Dataset/VOCdevkit/VOC2012/ -dataset_name pascal -n_shots 1 -bs 4 -ckpt stability_exps/test3/ -reproducability 1 -num_epoch 20
